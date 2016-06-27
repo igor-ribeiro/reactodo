5f1866fb5c0a5f3fb6fc128ef91715cb80713f04
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2016-06-27 14:56:04 -0300

    criando componente de exemplo

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('just react it!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloComponent extends React.Component {
+  render() {
+    return <h1>Hello, React!</h1>;
+  }
+}
+
+ReactDOM.render(<HelloComponent />, document.getElementById('app'));
